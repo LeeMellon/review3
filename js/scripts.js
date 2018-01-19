40045d7f478ca4e29b68118cd035f91c14fa2fec
Ian Goodrich <iangoodrich@hotmail.com>
2018-01-19 09:54:27 -0800

    business logic done?

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
+
+var removeThisVar = 16;
+
+function halCheck(usersNumber){
+  for (range = 0; range <= usersNumber; range ++) {
+    if (range % 3 === 0){
+      var userOutput = "I'm Sorry Dave, I can't do that."
+      console.log(userOutput);
+    } else if (((range.toString()).includes("1")) === true){
+      var userOutput = "BOOP";
+      console.log(userOutput);
+    } else if (((range.toString()).includes("0")) === true){
+      var userOutput = "BEEP";
+    } else {
+      var userOutput = range;
+      console.log(userOutput);
+
+    }
+  }
+
+};
+
 $(function(){
 
 
 
-
+halCheck(removeThisVar);
 });
